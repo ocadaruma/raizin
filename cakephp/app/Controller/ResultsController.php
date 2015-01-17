7a1e318703ba15bd4afcb2ce3cd5687563b09d8d
loop99 <loop99@infoseek.jp>
2015-01-17 17:46:22 +0900

    一応できた？

--- cakephp/app/Controller/ResultsController.php
+++ cakephp/app/Controller/ResultsController.php
@@ -48,7 +48,7 @@ class ResultsController extends AppController {
 		
 		$result = array('a' => 9, 'b' => 10, 'c' => 1);
 		arsort($result);
-		$this->set('result', key(array_slice($result, 0, 1)));
+		$this->set('result', array_shift($this->request->data));
 		
 	}
 }
